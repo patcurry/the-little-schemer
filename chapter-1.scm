e907d77d487753ce0c72122084e9586816f7b818
patcurry <currypat1985@gmail.com>
2018-01-16 13:20:02 +0100

    going through atoms, lists, and s-expressions

---
+++ chapter-1.scm
@@ -0,0 +1,60 @@
+;; Chapter 1 - Toys
+
+;; Is it true that this is an atom?
+(quote atom) 
+'atom 
+;; Yes, because atom is a string of characters beginning with the letter a.
+
+;; Is it true that this is an atom?
+(quote turkey)
+'turkey
+;; Yes, because turkey is a string of characters beginning with a letter.
+
+;; Is it true that this is an atom?
+1492
+;; Yes, because 1492 is a string of digits.
+
+;; Is it true that this is an atom?
+(quote u) 
+'u
+;; Yes, because u is a string of one character, which is a letter
+
+;; Is it true that this is an atom?
+(quote *abc$)
+'*abc$
+;; Yes, because *abc$ is a string of characters beginning with a letter
+;; or special character other than a left parentheses or right parentheses.
+
+;; Is it true that this is a list?
+(quote (atom)) 
+'(atom) 
+;; Yes, because (atom) is an atom enclosed by parentheses.
+
+;; Is it true that this is a list?
+'(atom turkey or)
+;; Yes, because is is a collection of attomes enclosed by parentheses.
+
+;; Is it true that this is a list?
+'(atom turkey) or
+;; No, because these are two S-expressions not enclosed by parentheses.
+;; The first one is a list containing two atoms, and the second is an atom.
+
+;; Is it true that this is a list?
+'((atom turkey) or)
+;; Yes, because the two S-expressions are now enclosed by parentheses.
+
+;; Is it true that this is an S-expression?
+'xyz
+;; Yes, because atoms are S-expressions.
+
+;; Is it true that this is an S-expression?
+'(x y z)
+;; Yes, because it is a list.
+
+;; Is it true that this is an S-expression?
+'((x y) z)
+;; Yes, because all lists are S-expressions.
+
+;; Is it true that this is a list?
+'(how are you doing so far)
+;; Yes, because it is a collection of S-expressions enclosed by parentheses.
